edeb35dfbf41219e1a4d0a6cfc74374aaf171fda
Michael <171223687+Bazorko@users.noreply.github.com>
2025-01-15 05:37:27 -0500

    - Added dependencies to test code.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(() => ({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/setup.js',
+  },
+}));
