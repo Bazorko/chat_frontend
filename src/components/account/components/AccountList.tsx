ebdf214183b018bf15e32527518930f86dbb2238
Michael <171223687+Bazorko@users.noreply.github.com>
2025-05-27 19:18:46 -0400

    - Extracted logout functionality into its own file, to reuse in verify email component. - Refactored the AccountList component to reuse handleLogout code.

--- src/components/account/components/AccountList.tsx
+++ src/components/account/components/AccountList.tsx
@@ -1,21 +1,14 @@
-import { useAuth } from "../../../hooks/useAuth";
 import { useData } from "../../../hooks/useData";
-import { disconnect } from "../../../socketstuff/ConnectionManager";
+import handleLogout from "../../../utils/auth/Logout";
 
 const AccountList = () => {
-    const { logout } = useAuth();
     const { user } = useData();
-    const handleLogout = () => {
-        disconnect();
-        localStorage.removeItem("user");
-        logout();
-    }
     return (
         <section className="self-center">
             <p className="text-white text-center text-2xl p-5">{ user.username }'s Account</p>
             <ul className="p-5 text-white text-center">
                 <li className="p-5">Change Password</li>
-                <li className="p-5" onClick={() => handleLogout()}>Sign Out</li>
+                <li className="p-5" onClick={handleLogout}>Sign Out</li>
             </ul>
         </section>
     )
--- src/utils/auth/VerifyEmail.tsx
+++ src/utils/auth/VerifyEmail.tsx
@@ -1,15 +1,32 @@
-import { disconnect } from "../../socketstuff/ConnectionManager";
-import { useAuth } from "../../hooks/useAuth";
+import { useState } from "react";
+import ErrorMessage from "../../components/account/components/ErrorMessage";
+import handleLogout from "./Logout";
+import { getAuth, sendEmailVerification } from "firebase/auth";
+
+interface ErrorData{
+    ok: boolean | undefined,
+    message: string
+}
+
 const VerifyEmail = () => {
-    const { logout } = useAuth();
-    const handleLogout = () => {
-        disconnect();
-        localStorage.removeItem("user");
-        logout();
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    const [ errorData, setErrorData ] = useState<ErrorData | undefined>(undefined);
+
+    const handleResendEmailVerification = () => {
+        if(user) {
+            sendEmailVerification(auth.currentUser);
+            setErrorData({ ok: true, message: "Email has been resent." });
+        } else {
+            setErrorData({ ok: false, message: "An error has occured, try again later." });
+        }
     }
     return(
         <div className="h-screen flex flex-col justify-center items-center">
             <p className="text-white pb-2">Please verify your email before continuing.</p>
+            { errorData && <ErrorMessage data={ errorData }/> }
+            <button className="text-white bg-primary_blue border-primary_blue rounded-lg p-3 hover:bg-primary_blue_darker" onClick={handleResendEmailVerification}>Resend Email</button>
             <p className="text-white pt-2 cursor-pointer hover:underline" onClick={() => handleLogout()}> Go back?</p>
         </div>
     );
