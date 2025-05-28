b4bf128cdb7f8e9b816fb850e3b8733e3ede0aee
Michael <171223687+Bazorko@users.noreply.github.com>
2025-05-28 15:29:06 -0400

    - Added a message to the AccountList component to be displayed on certain actions.

--- src/components/account/components/AccountList.tsx
+++ src/components/account/components/AccountList.tsx
@@ -1,16 +1,29 @@
+import { useState } from "react";
 import { useData } from "../../../hooks/useData";
 import { useResetPassword } from "../../../hooks/useResetPassword";
 import { useHandleLogout } from "../../../hooks/useHandleLogout";
+import ErrorMessage from "./ErrorMessage";
+
+interface ErrorData{
+    ok: boolean | undefined,
+    message: string
+}
 
 const AccountList = () => {
     const { user } = useData();
     const resetPassword = useResetPassword();
     const handleLogout = useHandleLogout();
+
+    const [ errorData, setErrorData ] = useState<ErrorData | undefined>(undefined);
     return (
         <section className="self-center">
             <p className="text-white text-center text-2xl p-5">{ user.username }'s Account</p>
+            { errorData && <ErrorMessage data={errorData}/> }
             <ul className="p-5 text-white text-center">
-                <li className="p-5 cursor-pointer" onClick={resetPassword}>Change Password</li>
+                <li className="p-5 cursor-pointer" onClick={() => { 
+                    resetPassword();
+                    setErrorData({ ok: true, message: "A password reset link was sent to your email." }); 
+                } }>Change Password</li>
                 <li className="p-5 cursor-pointer" onClick={handleLogout}>Sign Out</li>
             </ul>
         </section>
